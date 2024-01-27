a4bc792937c763823f4cfe3897e3efbdc03dc693
Krzysztof Gornowicz <krzysztofgornowicz@gmail.com>
2024-01-27 12:58:07 +0100

    Added targets Progress bars on targets

--- Jester/Assets/KinectGame/ObjectsGenerator.cs
+++ Jester/Assets/KinectGame/ObjectsGenerator.cs
@@ -8,21 +8,10 @@ using static Assets.KinectGame.Enums;
 public class ObjectsGenerator : MonoBehaviour
 {
     // Start is called before the first frame update
-    public GameObject TargetPrefab;
+    public  GameObject TargetPrefab;
     private GameObject[] Targets = new GameObject[4];
-    private bool redrawObjects = true;
-    private List<Vector3[]> TargetPositions = new List<Vector3[]>()
-    {
-        //                       LH,                RH,              LF,                RF
-        //new Vector3[4] { new Vector3(1.3f,2), new Vector3(-1.3f,2), new Vector3(-1.3f,-3), new Vector3(1.3f,-3), }, //cross hands
-        new Vector3[4] { new Vector3(-1.3f, 2), new Vector3(1.3f,2), new Vector3(-1.3f,-3), new Vector3(1.3f,-3)  }, //spread
-        new Vector3[4] { new Vector3(-2f,0), new Vector3(2f, 0), new Vector3(0f,-3), new Vector3(1.3f,-2)  }, //RF up
-        new Vector3[4] { new Vector3(-2f,2), new Vector3(2f, 2), new Vector3(-0.5f,-3), new Vector3(0.5f,-3)  }, //Y
-        new Vector3[4] { new Vector3(-0.5f,2), new Vector3(0.5f, 2), new Vector3(-0.5f,-3), new Vector3(0.5f,-3)  }, //M
-        new Vector3[4] { new Vector3(1f,0), new Vector3(2f, 0), new Vector3(-0.5f,-3), new Vector3(0.5f,-3)  }, //C
-        new Vector3[4] { new Vector3(-0.5f,3), new Vector3(0.5f, 3), new Vector3(-0.5f,-3), new Vector3(0.5f,-3)  }, //A
 
-    };
+
 
     void Start()
     {
@@ -32,84 +21,16 @@ public class ObjectsGenerator : MonoBehaviour
     // Update is called once per frame
     void FixedUpdate()
     {
-        if (redrawObjects)
+        for(int i = 0; i!= Targets.Length; i++)
         {
-            redrawObjects = false;
-            RestartTargets(true);
-        }
-        TriggerTargets();
-        redrawObjects = CheckIfAllTargetsDestroyed();
-    }
-
-    void RestartTargets(bool usePresets)
-    {
-        if (usePresets)
-        {
-            int selectedPreset = UnityEngine.Random.Range(0, TargetPositions.Count);
-            for (int i = 0; i != Targets.Length; i++)
+            if (Targets[i] == null || Targets[i].IsDestroyed())
             {
-                if (Targets[i] == null || Targets[i].GetComponent<TargetController>().shouldDestroy)
-                {
-                    GameObject targetToDelete = Targets[i];
-                    LimbType lt = (LimbType)(i + 1);
-                    Targets[i] = Instantiate(TargetPrefab);
-                    if (targetToDelete != null)
-                    {
-                        Destroy(targetToDelete);
-                    }
-                    Targets[i].name = "Target" + lt;
-                    TargetController tc = Targets[i].GetComponent<TargetController>();
-                    tc.transform.position = TargetPositions[selectedPreset][i];
-                    tc.TargetLimbType = lt;
-                    tc.shouldRandomize = false;
-                }
+                LimbType lt = (LimbType)(i + 1);
+                Targets[i] = Instantiate(TargetPrefab);
+                Targets[i].name = "Target" + lt;
+                Targets[i].GetComponent<TargetController>().TargetLimbType = lt;
             }
         }
-        else
-        {
-            for (int i = 0; i != Targets.Length; i++)
-            {
-                if (Targets[i] == null || Targets[i].GetComponent<TargetController>().shouldDestroy)
-                {
-                    LimbType lt = (LimbType)(i + 1);
-                    Targets[i] = Instantiate(TargetPrefab);
-                    Targets[i].name = "Target" + lt;
-                    TargetController tc = Targets[i].GetComponent<TargetController>();
-                    tc.TargetLimbType = lt;
-                    tc.shouldRandomize = true;
-                }
-            }
-        }
-    }
-
-    bool CheckIfAllTargetsDestroyed()
-    {
-        for (int i = 0; i != Targets.Length; i++)
-        {
-            if (!Targets[i].GetComponent<TargetController>().shouldDestroy)
-                return false;
-        }
-        Debug.Log("BRAWO!");
-        return true;
-    }
-
-    void TriggerTargets()
-    {
-        bool success = true;
-        for (int i = 0; i != Targets.Length; i++)
-        {
-            success &= Targets[i].GetComponent<TargetController>().IsTriggered();
-        }
-        for (int i = 0; i != Targets.Length; i++)
-        {
-            if (success)
-            {
-                Targets[i].GetComponent<TargetController>().StartProgressBar();
-            }
-            else
-            {
-                Targets[i].GetComponent<TargetController>().StopProgressBar();
-            }
-        }
+        
     }
 }
--- Jester/Assets/KinectGame/TargetController.cs
+++ Jester/Assets/KinectGame/TargetController.cs
@@ -8,40 +8,18 @@ using static Assets.KinectGame.Enums;
 public class TargetController : MonoBehaviour
 {
     public LimbType TargetLimbType;
-    public float timeToScore = 1f;
+    public float timeToScore = 5f;
     public GameObject ProgressBar;
-    public bool shouldRandomize = false;
     
     private Guid triggerGuid;
     private float timeFromTrigger = 0f;
-    private bool startProgressBar = false;
-    public bool shouldDestroy = false;
 
-    public bool IsTriggered()
-    {
-        return triggerGuid != Guid.Empty;
-    }
 
-    public void StartProgressBar()
-    {
-        StartCoroutine(Collect(triggerGuid));
-    }
-
-    public void StopProgressBar()
-    {
-        timeFromTrigger = 0f;
-        ProgressBar.GetComponent<Image>().fillAmount = 0;
-        startProgressBar = false;
-        shouldDestroy = false;
-    }
 
     // Start is called before the first frame update
     void Start()
     {
-        if (shouldRandomize)
-        {
-            this.transform.position = new Vector3(UnityEngine.Random.Range(-2f, 3), UnityEngine.Random.Range(-2f, 3), 0);
-        }
+        this.transform.position = new Vector3(UnityEngine.Random.Range(-2f, 3), UnityEngine.Random.Range(-2f, 3), 0);
         this.transform.localScale = Vector3.one * 0.5f;
         Color color = LimbTypeToColor(TargetLimbType);
         if (this.GetComponent<MeshRenderer>() != null)
@@ -93,21 +71,21 @@ public class TargetController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (startProgressBar)
+        if (triggerGuid != Guid.Empty)
         {
             timeFromTrigger += Time.deltaTime;
-            ProgressBar.GetComponent<Image>().fillAmount = timeFromTrigger / timeToScore;
         }
+        ProgressBar.GetComponent<Image>().fillAmount = timeFromTrigger / timeToScore;
     }
 
     IEnumerator Collect(Guid guid)
     {
-        startProgressBar = true;
         triggerGuid = guid;
         yield return new WaitForSeconds(timeToScore);
         if (triggerGuid == guid)
         {
-            shouldDestroy = true;
+            Debug.Log("Zlapane!");
+            Destroy(this.gameObject);
         }
     }
 
@@ -116,8 +94,7 @@ public class TargetController : MonoBehaviour
         Limb limb = other.gameObject.GetComponent<Limb>();
         if (limb != null && limb.LimbType == TargetLimbType)
         {
-            triggerGuid = Guid.NewGuid();
-            StartProgressBar();
+            StartCoroutine(Collect(Guid.NewGuid()));
         }
     }
 
@@ -127,7 +104,7 @@ public class TargetController : MonoBehaviour
         if (limb != null && limb.LimbType == TargetLimbType)
         {
             triggerGuid = Guid.Empty;
-            StopProgressBar();
+            timeFromTrigger = 0f;
         }
     }
 }
