197540b8760f8007dba88f425b90d1f047c376d0
unknown <jakub.luczejko@gmail.com>
2024-01-27 18:34:32 +0100

    stage 2 of VixaHere

--- Jester/Assets/VixaHero/SpellControllerScript.cs
+++ Jester/Assets/VixaHero/SpellControllerScript.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class SpellControllerScript : MonoBehaviour
 {
+    public int WhoAmI = 0;
+    private VivaInput input;
+    private bool IsInTrigger = false;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -14,5 +18,43 @@ public class SpellControllerScript : MonoBehaviour
     void Update()
     {
         
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        input.Enable();
+        IsInTrigger = true;
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        IsInTrigger = false;
+    }
+
+    private void Awake()
+    {
+        input = new VivaInput();
+    }
+
+    private void OnEnable()
+    {
+        input.Viixa.Key1.performed += ((c) => OnInputPress(1));
+        input.Viixa.Key2.performed += ((c) => OnInputPress(2));
+        input.Viixa.Key3.performed += ((c) => OnInputPress(3));
+        input.Viixa.Key4.performed += ((c) => OnInputPress(4));
+    }
+
+    private void OnDisable()
+    {
+        input.Disable();
+    }
+
+    public void OnInputPress(int inp)
+    {
+        if (IsInTrigger)
+        {
+            Debug.Log($"woop: {inp}");
+            input.Disable();
+        }
     }
 }
--- Jester/Assets/VixaHero/SpellMoveScript.cs
+++ Jester/Assets/VixaHero/SpellMoveScript.cs
@@ -1,19 +1,16 @@
-using System.Collections;
-using System.Collections.Generic;
-using System.Linq;
 using UnityEngine;
 
 public class SpellMoveScript : MonoBehaviour
 {
     public float moveSpeed = 5;
     public float deadZone = -16;
-    public GameObject[] pointers;
+    public GameObject[] sprites;
+
 
     // Start is called before the first frame update
     void Start()
     {
-        var sr = this.GetComponent<SpriteRenderer>();
-        pointers = GameObject.FindGameObjectsWithTag("PointerTag");
+
     }
 
     // Update is called once per frame
@@ -21,63 +18,9 @@ public class SpellMoveScript : MonoBehaviour
     {
         transform.position = transform.position + (Vector3.left * moveSpeed) * Time.deltaTime;
 
-        if (keyToPress != KeyCode.None)
-        {
-            if (Input.GetKeyDown(keyToPress))
-            {
-                Debug.Log("noice ! ", this);
-                keyToPress = KeyCode.None;
-            }
-        }
-
         if (transform.position.x < deadZone)
         {
             Destroy(gameObject);
         }
-    }
-
-    //private bool IsInTrigger = false;
-    private KeyCode keyToPress = KeyCode.None;
-
-    private void OnTriggerEnter2D(Collider2D collision)
-    {
-        //keyToPress = collision.gameObject switch
-        //{
-        //    pointers[0]  => KeyCode.Alpha1,
-        //    pointers[1] => KeyCode.Alpha2,
-        //    _ => KeyCode.None
-        //}; ;
-
-        if (collision.gameObject == pointers[0])
-        {
-            keyToPress = KeyCode.Alpha4;
-        }
-        else if (collision.gameObject == pointers[1])
-        {
-            keyToPress = KeyCode.Alpha3;
-        }
-        else if (collision.gameObject == pointers[2])
-        {
-            keyToPress = KeyCode.Alpha2;
-        }
-        else if (collision.gameObject == pointers[3])
-        {
-            keyToPress = KeyCode.Alpha1;
-        }
-        else
-        {
-            Debug.Log("DUPAAA");
-        }
-        //IsInTrigger = true;   
-    }
-    private void OnTriggerExit2D(Collider2D collision)
-    {
-        keyToPress = KeyCode.None;
-        //IsInTrigger = false;
-    }
-
-    private void OnTriggerStay2D(Collider2D collision)
-    {
-        
     }
 }
