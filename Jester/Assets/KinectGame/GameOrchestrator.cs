0367e4152be480d319d2a422e4e87391a791cc77
Krzysztof Gornowicz <krzysztofgornowicz@gmail.com>
2024-01-28 10:50:36 +0100

    Upgraded EndScreen options based on the score

--- Jester/Assets/KinectGame/GameOrchestrator.cs
+++ Jester/Assets/KinectGame/GameOrchestrator.cs
@@ -4,6 +4,7 @@ using TMPro;
 using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class GameOrchestrator : MonoBehaviour
 {
@@ -12,9 +13,12 @@ public class GameOrchestrator : MonoBehaviour
     public GameObject TimeLeftText;
     public GameObject ObjectsGenerator;
     public GameObject Kween;
+    public GameObject EndScreen;
 
     private float ElapsedTime = 0f;
     private bool isRunning = false;
+    private int score = 0;
+    private float targetScore = 100f;
     private SpriteRenderer happyKweenSpriteRenderer;
     // Start is called before the first frame update
     void Start()
@@ -26,14 +30,28 @@ public class GameOrchestrator : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (!isRunning)
+        {
+            if (Input.GetKeyDown(KeyCode.Return))
+            {
+                ElapsedTime = 0f;
+                ObjectsGenerator.GetComponent<ObjectsGenerator>().score = 0;
+                isRunning = true;
+            }
+        }
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            StopGame();
+            //SceneManager.LoadScene("MainMap");
+        }
     }
 
     private void FixedUpdate()
     {
-        int score = ObjectsGenerator.GetComponent<ObjectsGenerator>().score;
+        score = ObjectsGenerator.GetComponent<ObjectsGenerator>().score;
         TimeLeftText.GetComponent<TextMeshProUGUI>().text = $"Time left: {TimeLimit - ElapsedTime:00.0}";
         ScoreText.GetComponent<TextMeshProUGUI>().text = $"Score: {score}";
-        happyKweenSpriteRenderer.color = new Color(happyKweenSpriteRenderer.color.r, happyKweenSpriteRenderer.color.g, happyKweenSpriteRenderer.color.b, score/30f);
+        happyKweenSpriteRenderer.color = new Color(happyKweenSpriteRenderer.color.r, happyKweenSpriteRenderer.color.g, happyKweenSpriteRenderer.color.b, score/targetScore);
         ObjectsGenerator.GetComponent<ObjectsGenerator>().isRunning = isRunning;
         if(isRunning)
         {
@@ -43,24 +61,51 @@ public class GameOrchestrator : MonoBehaviour
                 StopGame();
             }
         }
-        else
-        {
-            if(Input.GetKeyDown(KeyCode.Return))
-            {
-                ElapsedTime = 0f;
-                ObjectsGenerator.GetComponent<ObjectsGenerator>().score = 0;
-                isRunning = true;
-            }
-        }
-        if(Input.GetKeyDown(KeyCode.Escape))
-        {
-            StopGame();
-            SceneManager.LoadScene("MainMap");
-        }
+        //else
+        //{
+        //    if(Input.GetKeyDown(KeyCode.Return))
+        //    {
+        //        ElapsedTime = 0f;
+        //        ObjectsGenerator.GetComponent<ObjectsGenerator>().score = 0;
+        //        isRunning = true;
+        //    }
+        //}
+        //if(Input.GetKeyDown(KeyCode.Escape))
+        //{
+        //    StopGame();
+        //    //SceneManager.LoadScene("MainMap");
+        //}
     }
 
     private void StopGame()
     {
         isRunning = false;
+        ShowEndScreen();
     }
+
+    private void ShowEndScreen()
+    {
+        EndScreen.SetActive(true);
+        var happySprite = EndScreen.transform.FindChild("PersonHappy").GetComponent<SpriteRenderer>();
+        happySprite.color = new Color(happySprite.color.r, happySprite.color.g, happySprite.color.b, score / targetScore);
+        if(score >= targetScore/2)
+        {
+            EndScreen.GetComponentInChildren<Text>().text = successText;
+        }
+        else
+        {
+            EndScreen.GetComponentInChildren<Text>().text = failureText;
+        }
+    }
+
+    private const string successText = @"The Queen:
+""Oh, Jester, you are a marvel! Your dance and acrobatics have broken the chains of sorrow that bound me. I haven't laughed like this in ages. Thank you for bringing color back to my world and joy to my heart.""
+
+
+Jester to the Queen:
+""My body is ready! An entire night of acrobatics and singing wouldn't wear me down for you My Queen! <3""";
+    
+    
+    private const string failureText = @"The Queen:
+""Oh Jester, your efforts are amendable. Will you dance for me more?""";
 }
